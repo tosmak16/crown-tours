187f9cdbab1163fc6c6755120e4223d0896a855e
Tosmak <tosmak16@gmail.com>
2019-12-04 12:12:32 +0100

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Link } from "gatsby";
+
 import Layout from "../components/Layout";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 export default () => (
   <div>
-    <Layout>Hello world!</Layout>
+    <Layout>
+      <Hero>
+        <Banner
+          title="continue exploring"
+          info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+        >
+          <Link to="/tours" className="btn-white">
+            explore tours
+          </Link>
+        </Banner>
+      </Hero>
+    </Layout>
   </div>
 );
