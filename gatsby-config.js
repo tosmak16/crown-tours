f317072f800fc5ca19003cb9e4e1e0fb4609867f
Tosmak <tosmak16@gmail.com>
2019-12-05 07:40:51 +0100

    Update plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+  siteMetadata: {
+    title: "Crown-tours",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: "@Tosmak",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`,
+  ],
 };
