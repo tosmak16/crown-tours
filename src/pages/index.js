7139670f9bd97bd86b1abb5a1f7314cd273f25fc
Tosmak <tosmak16@gmail.com>
2019-12-05 07:43:53 +0100

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,36 @@
 import React from "react";
-import { Link } from "gatsby";
+import { graphql } from "gatsby";
+import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 import Layout from "../components/Layout";
-import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import About from "../components/About";
 import Services from "../components/Services";
-
-export default () => (
-  <div>
-    <Layout>
-      <Hero>
-        <Banner
-          title="continue exploring"
-          info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
-        >
-          <Link to="/tours" className="btn-white">
-            explore tours
-          </Link>
-        </Banner>
-      </Hero>
-      <About />
-      <Services />
-    </Layout>
-  </div>
+import Hero from "../components/Hero";
+export default ({ data }) => (
+  <Layout>
+    <Hero home="true" img={data.defaultBcg.childImageSharp.fluid}>
+      <Banner
+        title="continue exploring"
+        info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+      >
+        <AniLink fade to="/tours" className="btn-white">
+          explore tours
+        </AniLink>
+      </Banner>
+    </Hero>
+    <About />
+    <Services />
+  </Layout>
 );
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
