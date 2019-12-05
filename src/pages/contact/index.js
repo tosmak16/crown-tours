3a1048358ffac33632b7be5255659ef5def6910e
Tosmak <tosmak16@gmail.com>
2019-12-05 07:42:52 +0100

    Add contact form and page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,7 +1,27 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 
-const contact = () => {
-  return <div>Contact</div>
+import Layout from "../../components/Layout";
+import Hero from "../../components/Hero";
+import ContactForm from "../../components/ContactForm";
+
+export default function contact({ data }) {
+  return (
+    <Layout>
+      <Hero img={data.contactBcg.childImageSharp.fluid} />
+      <ContactForm />
+    </Layout>
+  );
 }
 
-export default contact
+export const query = graphql`
+  query {
+    contactBcg: file(relativePath: { eq: "connectBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
