e6c392ec73730a9db5029e64e31854942cdaf679
Tosmak <tosmak16@gmail.com>
2019-12-05 07:44:02 +0100

    Update tours

--- src/pages/tours/index.js
+++ src/pages/tours/index.js
@@ -1,7 +1,26 @@
-import React from "react"
+import React, { Component } from "react";
+import { graphql } from "gatsby";
 
-const tours = () => {
-  return <div>Tours</div>
+import Layout from "../../components/Layout";
+import Hero from "../../components/Hero";
+export default class tours extends Component {
+  render() {
+    return (
+      <Layout>
+        <Hero img={this.props.data.defaultBcg.childImageSharp.fluid} />
+      </Layout>
+    );
+  }
 }
 
-export default tours
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
