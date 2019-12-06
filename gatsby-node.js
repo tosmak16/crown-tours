ab70ef57940b624501d45e968a3798657c90a80b
Tosmak <tosmak16@gmail.com>
2019-12-06 07:30:26 +0100

    Add gatsby-node config

---
+++ gatsby-node.js
@@ -0,0 +1,26 @@
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const { data } = await graphql(`
+    query {
+      tours: allContentfulTours {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  data.tours.edges.forEach(({ node }) => {
+    createPage({
+      path: `tours/${node.slug}`,
+      component: path.resolve("./src/components/SingleTourTemplate/index.js"),
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+}
