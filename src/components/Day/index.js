5dfab93e1f6ae23e099d4f47c59d7da544117c2c
Tosmak <tosmak16@gmail.com>
2019-12-06 07:30:37 +0100

    Add tour details page

---
+++ src/components/Day/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { FaAngleDown } from "react-icons/fa";
+
+import styles from "./day.module.scss";
+const Day = ({ day, info }) => {
+  const [showInfo, setInfo] = useState(false);
+  const toggleInfo = () => {
+    setInfo(showInfo => !showInfo);
+  };
+  return (
+    <article className={styles.day}>
+      <h4>
+        {day}
+        <button className={styles.btn} onClick={toggleInfo}>
+          <FaAngleDown />
+        </button>
+      </h4>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default Day;
---
+++ src/components/SingleTourTemplate/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import { FaMoneyBillWave, FaMap } from "react-icons/fa";
+import AniLink from "gatsby-plugin-transition-link/AniLink";
+
+import Layout from "../Layout";
+import Hero from "../Hero";
+import Day from "../Day";
+
+import styles from "./single.tour.module.scss";
+
+const Template = ({ data }) => {
+  const {
+    name,
+    price,
+    country,
+    days,
+    description: { description },
+    images,
+    start,
+    journey,
+  } = data.tour;
+
+  const [mainImage, ...tourImages] = images;
+
+  return (
+    <Layout>
+      <Hero img={mainImage.fluid} />
+      <section className={styles.template}>
+        <div className={styles.center}>
+          <div className={styles.images}>
+            {tourImages.map((item, index) => {
+              return (
+                <Img
+                  key={index}
+                  fluid={item.fluid}
+                  alt="single tour"
+                  className={styles.image}
+                />
+              );
+            })}
+          </div>
+          <h2>{name}</h2>
+          <div className={styles.info}>
+            <p>
+              <FaMoneyBillWave className={styles.icon} />
+              starting from ${price}
+            </p>
+            <p>
+              <FaMap className={styles.icon} />
+              {country}
+            </p>
+          </div>
+          <h4>starts on : {start}</h4>
+          <h4>duration : {days} days</h4>
+          <p className={styles.desc}>{description}</p>
+          <h2>daily schedule</h2>
+          <div className={styles.journey}>
+            {journey.map((item, index) => {
+              return <Day key={index} day={item.day} info={item.info} />;
+            })}
+          </div>
+          <AniLink fade to="/tours" className="btn-primary">
+            back to tours
+          </AniLink>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTours(slug: { eq: $slug }) {
+      name
+      price
+      country
+      days
+      start(formatString: "dddd MMMM Do, YYYY")
+      description {
+        description
+      }
+      journey {
+        day
+        info
+      }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`;
+
+export default Template;
